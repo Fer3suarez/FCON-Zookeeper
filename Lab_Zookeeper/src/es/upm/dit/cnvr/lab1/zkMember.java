ff8768d2a923f54c6720e37032920751c0d6b013
Fernando Suárez <suarez11fer@gmail.com>
2020-11-02 17:22:14 +0100

    Implementado cerrojo distribuido para elección del lider por contador. Falla

--- Lab_Zookeeper/src/es/upm/dit/cnvr/lab1/zkMember.java
+++ Lab_Zookeeper/src/es/upm/dit/cnvr/lab1/zkMember.java
@@ -4,6 +4,7 @@ import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.util.Random;
 
 import org.apache.zookeeper.CreateMode;
@@ -84,7 +85,6 @@ public class zkMember implements Watcher{
 				List<String> list = zk.getChildren(rootMembers, watcherMember, s); //this, s);
 				System.out.println("Created znode nember id:"+ myId );
 				printListMembers(list);
-				esLider();
 			} catch (KeeperException e) {
 				System.out.println("The session with Zookeeper failes. Closing");
 				return;
@@ -112,9 +112,22 @@ public class zkMember implements Watcher{
 				System.out.println("        Update!!");
 				List<String> list = zk.getChildren(rootMembers,  watcherMember); //this);
 				printListMembers(list);
-				esLider();
 			} catch (Exception e) {
 				System.out.println("Exception: wacherMember");
+			}
+		}
+	};
+	
+	private Watcher  watcherLocker = new Watcher() { //Watcher para el cerrojo
+		public void process(WatchedEvent event) {
+			System.out.println("------------------Watcher Locker------------------\n");		
+			try {
+				System.out.println("        Update!!");
+				List<String> list = zk.getChildren(rootMembers,  watcherLocker); //this);
+				printListMembers(list);
+				esLider(1);
+			} catch (Exception e) {
+				System.out.println("Exception: wacherLocker");
 			}
 		}
 	};
@@ -140,30 +153,95 @@ public class zkMember implements Watcher{
 	}
 	
 	private String pathLider;
+	private static String pathLock = "/locknode";
+	private static int count = 0;
+	private Integer mutex = -1;
 	
-	private void esLider() {
+	private void esLider(int valor) {
 		try {
-			List<String> lista = zk.getChildren(rootMembers, false);
+			List<String> lista = zk.getChildren(pathLock, watcherMember);
 			Collections.sort(lista);
+			pathLider = pathLock + "/" + lista.get(0); // path del lider en el cerrojo
+			
+			Stat s = zk.exists(pathLock, watcherMember);
+			byte[] b = zk.getData(pathLock, watcherMember, s);
+			int valorContador = (ByteBuffer.wrap(b)).getInt(); //Obtenemos el ultimo valor del contador
+			
 			if(lista.indexOf(myId.substring(myId.lastIndexOf('/')+1)) != 0) {
-				pathLider = rootMembers + "/" + lista.get(0);
 				System.out.println("El lider es: " + lista.get(0));
-				zk.exists(pathLider, watcherMember);
+				System.out.println("Tengo que esperar para ser el lider...");
+				synchronized(mutex) {
+					zk.exists(pathLider, watcherLocker);
+					mutex.wait();
+				}
 			} else {
-				System.out.println("El nodo "+ myId + " es el lider");
+				System.out.println("Soy el lider");
+				count = valorContador+valor;
+				s = zk.exists(pathLider, watcherMember);
+				byte[] b2 = ByteBuffer.allocate(4).putInt(valorContador).array();
+				zk.setData(pathLock, b2, s.getVersion());
+				System.out.println("El contador vale: "+ valorContador);
+				
+				System.out.println("El contador ha aumentado y el lider va a dejar de ser: "+ myId);
+				s = zk.exists(pathLider, watcherMember);
+				System.out.println("Se va a borrar el lider");
+				zk.delete(pathLider, s.getVersion());
+				notify();
 			}
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
 	}
 	
+	private void addCounterValue(int valor) {
+		if(zk != null) {
+			try {
+				// Create a folder, if it is not created
+				String response = new String();
+				Stat s = zk.exists(rootMembers, watcherMember); //this);
+				if (s == null) {
+					// Created the znode, if it is not created.
+					byte[] b = ByteBuffer.allocate(4).putInt(0).array(); // Se crea un nodo lock con un contador a 0
+					response = zk.create(rootMembers, b, 
+							Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+					System.out.println(response);
+				}
+
+				// Create a znode for registering as member and get my id
+				myId = zk.create(pathLock + "/host-", new byte[0], 
+						Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+
+				myId = myId.replace(pathLock + "/", "");
+
+				System.out.println("Created znode nember id:"+ myId );
+				esLider(valor);
+			} catch (KeeperException e) {
+				System.out.println("The session with Zookeeper failes. Closing");
+				return;
+			} catch (InterruptedException e) {
+				System.out.println("InterruptedException raised");
+			}
+		}
+	}
+	
 	public static void main(String[] args) {
 		zkMember zk = new zkMember();
-		
-		try {
-			Thread.sleep(300000); 			
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
+		Thread thread = new Thread() {
+			public void run() {
+				for (int i = 0; i < 100; i++) {
+					System.out.println("Valor del contador: " + count);
+					zk.addCounterValue(1);
+					System.out.println("Valor actualizado: " + count);
+					try {
+						Thread.sleep(100); 			
+					} catch (Exception e) {
+						// TODO: handle exception
+					}
+				}
+				System.out.println("Contador final: " + count);
+				return;
+			}
+		};
+		thread.start();
 	}
 }
